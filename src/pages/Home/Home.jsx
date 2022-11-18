9462f42620bbacaf34a9cd797967b5882c8366d8
hoangdanh190797 <hoangdanh190797@gmail.com>
2022-11-18 07:19:38 +0700

    upload part 1

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+    );
+  }
+}
